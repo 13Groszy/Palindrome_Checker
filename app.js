5f6a64ad79c3118f2ebf5c9946b3057eff755a32
Mateusz Daszkiewicz <topdrive@Mini-Mateusz.lan>
2022-05-01 15:43:23 +0100

    Simple styling, app is working

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+let checkPalindrome =()=>{
+//selectors
+    const typedValue = document.querySelector('#input').value.split('');
+    const resultText = document.querySelector("#result");
 
+//Upper case prevent from checks - everything what is typed go to lower case
+    let toLowCase = typedValue.map(function(val){
+        return val.toLowerCase();
+    })
+
+//Turning array to string again
+    const lowCase = toLowCase.join("");
+    const reversed = toLowCase.reverse().join("");
+
+//Check if value is not empty
+if (reversed.length === 0) {
+    resultText.innerHTML = "Please type in the text"
+} else if(reversed.length === 1) {
+    resultText.innerHTML = "Please type in longer text"
+} else{
+     //Simple check and returning result
+     if(reversed === lowCase){
+        resultText.innerHTML = "It is a palindrome"
+    }else{
+        resultText.innerHTML = "It is not a palindrome"
+    }
+}
+}
